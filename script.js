44ed8b5d80e86b69276c4271191230308bcb797d
MariaAlfo <123668220+MariaAlfo@users.noreply.github.com>
2023-01-26 10:53:07 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,39 @@
+/* Declare variables below to save the different sections (divs) of your story*/
+let StoryPart2Italy = document.querySelector(".option-one-screen");
+let StoryPart2Colombia = document.querySelector(".option-two-screen");
+let StoryPart3Italy = document.querySelector(".option-one-end");
+let StoryPart3Colombia = document.querySelector(".option-two-end");
+let ItalyButton= document.querySelector(".option-one");
+let ColombiaButton=document.querySelector(".option-two");
+let NotTravelButton=document.querySelector(".option-three");
+let ItalianPicture=document.querySelector(".ItalianPicture");
+
+// When you're ready to make event handlers, uncomment the code below. Then fill in the blanks with the correct variables!
+
+
+ItalyButton.onclick=function(){
+StoryPart2Italy.style.display="block";
+
+};
+ColombiaButton.onclick=function(){
+StoryPart2Colombia.style.display="block";
+};
+
+    
+
+NotTravelButton.onclick=function(){
+ StoryPart3Italy.style.display="block";
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
